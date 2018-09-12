c9cf889744edfbd7369264eff2d9b052b7de3fc7
maruhachi <konchan1129@gmail.com>
2018-09-12 20:00:57 +0900

    Spring のユーティリティでさえ動いてくれない

--- src/test/java/tech/yko/javaspock/service/MailServiceImplTest.java
+++ src/test/java/tech/yko/javaspock/service/MailServiceImplTest.java
@@ -1,17 +1,25 @@
 package tech.yko.javaspock.service;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.rule.OutputCapture;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MailServiceImplTest {
 
     MailServiceImpl mailService = new MailServiceImpl();
 
+    @Rule
+    public OutputCapture capture = new OutputCapture();
+
     @Test
     void send() {
-        System.out.println("in Test code.");
+
         mailService.send("in the implementation");
-        System.out.println("test end.");
+
+        assertEquals(capture.toString(), "msg is in the implementation in the implementation");
     }
 }
